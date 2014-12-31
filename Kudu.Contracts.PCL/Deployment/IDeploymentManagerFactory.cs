723efbd8bd9c7eda46ae7444c0e3e263316ad9a8
Emad Alashi <emad.ashi@gmail.com>
2014-12-31 12:05:02 +1100

    Deleted files not needed by the client

--- Kudu.Contracts.PCL/Deployment/IDeploymentManagerFactory.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Kudu.Core.PCL.Deployment
-{
-    public interface IDeploymentManagerFactory
-    {
-        IDeploymentManager CreateDeploymentManager();
-    }
-}
--- Kudu.Contracts.PCL/SourceControl/IFileFinder.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Kudu.Core.PCL.SourceControl
-{
-    public interface IFileFinder
-    {
-        IEnumerable<string> ListFiles(string path, SearchOption searchOption, params string[] lookupList);
-    }
-}
